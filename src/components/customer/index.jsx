7c2a767fc8972647d4e79466cdcb4cadc9a12d01
IzzatjonAkbarov <akbrv.i.006@gmail.com>
2025-01-27 12:34:05 +0500

    some changes added to ui responsive

--- src/components/customer/index.jsx
+++ src/components/customer/index.jsx
@@ -1,104 +1,132 @@
 import React from "react";
 import star from "../../assets/svg/star.svg";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import { Navigation, Pagination, Autoplay } from "swiper/modules";
+
 const Customer = () => {
   return (
     <div className="bg-[#f9f8f3] py-[47px]">
       <div className="container1">
         <h1 className="text-[35px] py-[30px]">Отзывы наших клиентов</h1>
       </div>
-      <div className="container1 ">
-        <div className="flex items-center justify-between gap-10 max-[1107px]:flex-col ">
-          <div className="p-[25px] bg-white border border-[#e8e8e8] rounded-b-md max-[350px]:p-[10px]">
-            <div className="border-b border-b-[#4f4d4d] py-[25px]   flex items-center justify-between">
-              <div className="">
-                <h4 className="font-nomral text-[18px]">Алексей С.А.</h4>
-                <p className="text-[14px] text-[#bbb]">28 июня 2021</p>
-              </div>
-              <div className="flex items-end  flex-col">
-                <div className="flex items-center gap-[5px]">
-                  <img src={star} alt="" />
-                  <img src={star} alt="" />
-                  <img src={star} alt="" />
-                  <img src={star} alt="" />
-                  <img src={star} alt="" />
+      <div className="container1">
+        <Swiper
+          modules={[Navigation, Pagination, Autoplay]}
+          navigation
+          pagination={{ clickable: true }}
+          autoplay={{ delay: 3000, disableOnInteraction: false }}
+          spaceBetween={20}
+          breakpoints={{
+            640: { slidesPerView: 1, spaceBetween: 20 },
+            768: { slidesPerView: 2, spaceBetween: 30 },
+            1024: { slidesPerView: 3, spaceBetween: 40 },
+          }}
+          loop={true}>
+          {/* Slide 1 */}
+          <SwiperSlide>
+            <div className="p-[25px] bg-white border border-[#e8e8e8] rounded-b-md max-[350px]:p-[10px]">
+              <div className="border-b border-b-[#4f4d4d] py-[25px] flex items-center justify-between">
+                <div>
+                  <h4 className="font-nomral text-[18px]">Алексей С.А.</h4>
+                  <p className="text-[14px] text-[#bbb]">28 июня 2021</p>
                 </div>
-                <p className="text-[13px]">Общая оценка: (4,8) </p>
-              </div>
-            </div>
-            <div>
-              <p className="font-thin text-[14px] py-[25px]">
-                «Цена хорошая, все соответствует ожиданиям. Подбирала перголу
-                для своего участка. Также мне хотелось бы отметить вежливость
-                работников компании, все подробно рассказали. Я считаю этот
-                магазин отличным. И наверняка буду покупать снова или советовать
-                коллегам.»
-              </p>
-              <a href="#" className="!underline text-[#ff7a1b]">
-                Читать полностью
-              </a>
-            </div>
-          </div>
-          <div className="p-[25px] bg-white border border-[#e8e8e8] rounded-b-md max-[350px]:p-[10px]">
-            <div className="border-b border-b-[#4f4d4d] py-[25px]   flex items-center justify-between">
-              <div className="">
-                <h4 className="font-nomral text-[18px]">Алексей С.А.</h4>
-                <p className="text-[14px] text-[#bbb]">28 июня 2021</p>
-              </div>
-              <div className="flex items-end  flex-col">
-                <div className="flex items-center gap-[5px]">
-                  <img src={star} alt="" />
-                  <img src={star} alt="" />
-                  <img src={star} alt="" />
-                  <img src={star} alt="" />
-                  <img src={star} alt="" />
+                <div className="flex items-end flex-col">
+                  <div className="flex items-center gap-[5px]">
+                    <img src={star} alt="" />
+                    <img src={star} alt="" />
+                    <img src={star} alt="" />
+                    <img src={star} alt="" />
+                    <img src={star} alt="" />
+                  </div>
+                  <p className="text-[13px]">Общая оценка: (4,8) </p>
                 </div>
-                <p className="text-[13px]">Общая оценка: (4,8) </p>
+              </div>
+              <div>
+                <p className="font-thin text-[14px] py-[25px]">
+                  «Цена хорошая, все соответствует ожиданиям. Подбирала перголу
+                  для своего участка. Также мне хотелось бы отметить вежливость
+                  работников компании, все подробно рассказали. Я считаю этот
+                  магазин отличным. И наверняка буду покупать снова или
+                  советовать коллегам.»
+                </p>
+                <a href="#" className="!underline text-[#ff7a1b]">
+                  Читать полностью
+                </a>
               </div>
             </div>
-            <div>
-              <p className="font-thin text-[14px] py-[25px]">
-                «Цена хорошая, все соответствует ожиданиям. Подбирала перголу
-                для своего участка. Также мне хотелось бы отметить вежливость
-                работников компании, все подробно рассказали. Я считаю этот
-                магазин отличным. И наверняка буду покупать снова или советовать
-                коллегам.»
-              </p>
-              <a href="#" className="!underline text-[#ff7a1b]">
-                Читать полностью
-              </a>
-            </div>
-          </div>
-          <div className="p-[25px] bg-white border border-[#e8e8e8] rounded-b-md max-[350px]:p-[10px]">
-            <div className="border-b border-b-[#4f4d4d] py-[25px]   flex items-center justify-between">
-              <div className="">
-                <h4 className="font-nomral text-[18px]">Алексей С.А.</h4>
-                <p className="text-[14px] text-[#bbb]">28 июня 2021</p>
-              </div>
-              <div className="flex items-end  flex-col">
-                <div className="flex items-center gap-[5px]">
-                  <img src={star} alt="" />
-                  <img src={star} alt="" />
-                  <img src={star} alt="" />
-                  <img src={star} alt="" />
-                  <img src={star} alt="" />
+          </SwiperSlide>
+
+          {/* Slide 2 */}
+          <SwiperSlide>
+            <div className="p-[25px] bg-white border border-[#e8e8e8] rounded-b-md max-[350px]:p-[10px]">
+              <div className="border-b border-b-[#4f4d4d] py-[25px] flex items-center justify-between">
+                <div>
+                  <h4 className="font-nomral text-[18px]">Алексей С.А.</h4>
+                  <p className="text-[14px] text-[#bbb]">28 июня 2021</p>
                 </div>
-                <p className="text-[13px]">Общая оценка: (4,8) </p>
+                <div className="flex items-end flex-col">
+                  <div className="flex items-center gap-[5px]">
+                    <img src={star} alt="" />
+                    <img src={star} alt="" />
+                    <img src={star} alt="" />
+                    <img src={star} alt="" />
+                    <img src={star} alt="" />
+                  </div>
+                  <p className="text-[13px]">Общая оценка: (4,8) </p>
+                </div>
+              </div>
+              <div>
+                <p className="font-thin text-[14px] py-[25px]">
+                  «Цена хорошая, все соответствует ожиданиям. Подбирала перголу
+                  для своего участка. Также мне хотелось бы отметить вежливость
+                  работников компании, все подробно рассказали. Я считаю этот
+                  магазин отличным. И наверняка буду покупать снова или
+                  советовать коллегам.»
+                </p>
+                <a href="#" className="!underline text-[#ff7a1b]">
+                  Читать полностью
+                </a>
               </div>
             </div>
-            <div>
-              <p className="font-thin text-[14px] py-[25px]">
-                «Цена хорошая, все соответствует ожиданиям. Подбирала перголу
-                для своего участка. Также мне хотелось бы отметить вежливость
-                работников компании, все подробно рассказали. Я считаю этот
-                магазин отличным. И наверняка буду покупать снова или советовать
-                коллегам.»
-              </p>
-              <a href="#" className="!underline text-[#ff7a1b]">
-                Читать полностью
-              </a>
+          </SwiperSlide>
+
+          {/* Slide 3 */}
+          <SwiperSlide>
+            <div className="p-[25px] bg-white border border-[#e8e8e8] rounded-b-md max-[350px]:p-[10px]">
+              <div className="border-b border-b-[#4f4d4d] py-[25px] flex items-center justify-between">
+                <div>
+                  <h4 className="font-nomral text-[18px]">Алексей С.А.</h4>
+                  <p className="text-[14px] text-[#bbb]">28 июня 2021</p>
+                </div>
+                <div className="flex items-end flex-col">
+                  <div className="flex items-center gap-[5px]">
+                    <img src={star} alt="" />
+                    <img src={star} alt="" />
+                    <img src={star} alt="" />
+                    <img src={star} alt="" />
+                    <img src={star} alt="" />
+                  </div>
+                  <p className="text-[13px]">Общая оценка: (4,8) </p>
+                </div>
+              </div>
+              <div>
+                <p className="font-thin text-[14px] py-[25px]">
+                  «Цена хорошая, все соответствует ожиданиям. Подбирала перголу
+                  для своего участка. Также мне хотелось бы отметить вежливость
+                  работников компании, все подробно рассказали. Я считаю этот
+                  магазин отличным. И наверняка буду покупать снова или
+                  советовать коллегам.»
+                </p>
+                <a href="#" className="!underline text-[#ff7a1b]">
+                  Читать полностью
+                </a>
+              </div>
             </div>
-          </div>
-        </div>
+          </SwiperSlide>
+        </Swiper>
         <div className="flex items-center justify-center my-[10px]">
           <button className="py-[10px] px-[33px] bg-[#ff7a1b] text-white hover:bg-white hover:text-[#ff7a1b] transition-all">
             Открыть все отзывы
--- src/components/showcase/index.jsx
+++ src/components/showcase/index.jsx
@@ -1,31 +1,92 @@
 import React from "react";
 import "./style.css";
-import grass from "../../assets/imgs/showcasegrass.png";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
 import showcasehome from "../../assets/imgs/showcasehome.png";
-
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css"; // Core Swiper styles
+import "swiper/css/navigation"; // Optional navigation styles
+import "swiper/css/pagination"; // Optional pagination styles
+import { Navigation, Pagination, Autoplay } from "swiper/modules";
 const Showcase = () => {
   return (
     <div className="bg-[#f9f8f3]">
       <div className=" container1 pt-5">
-        <div className="w-full showcase text-white relative">
-          <div className="py-[51px] px-[99px] max-[670px]:px-5">
-            <h1 className="text-[40px] max-[450px]:text-[30px]  max-[330px]:text-[25px]">
-              Беседки под ключ <br /> за 7 дней
-            </h1>
-          </div>
-          <div className="pb-[50px] relative z-10 max-[503px]:hidden">
-            <div className="flex items-center gap-14 text-[24px] py-[22px] px-[99px] max-[670px]:px-5 bg-[#ff7a1b]">
-              <p>Выгода до 24%</p>
-              <p>Сборка за 1 день</p>
+        <Swiper
+          modules={[Navigation, Pagination, Autoplay]} // Add required modules
+          pagination={{ clickable: true }}
+          spaceBetween={30}
+          slidesPerView={1}
+          loop={true}
+          autoplay={{
+            delay: 2000,
+            disableOnInteraction: false,
+          }}>
+          <SwiperSlide>
+            <div className="w-full showcase text-white relative">
+              <div className="py-[51px] px-[99px] max-[670px]:px-5">
+                <h1 className="text-[40px] max-[450px]:text-[30px]  max-[330px]:text-[25px]">
+                  Беседки под ключ <br /> за 7 дней
+                </h1>
+              </div>
+              <div className="pb-[50px] relative z-10 max-[503px]:hidden">
+                <div className="flex items-center gap-14 text-[24px] py-[22px] px-[99px] max-[670px]:px-5 bg-[#ff7a1b]">
+                  <p>Выгода до 24%</p>
+                  <p>Сборка за 1 день</p>
+                </div>
+              </div>
+              <div className="h-[352px] w-[500px]  bg-white rounded-tl-full absolute right-0 top-0 max-[1050px]:hidden"></div>
+              <img
+                className="absolute top-0 right-10 z-20 max-[1050px]:hidden"
+                src={showcasehome}
+                alt=""
+              />
             </div>
-          </div>
-          <div className="h-[352px] w-[500px]  bg-white rounded-tl-full absolute right-0 top-0 max-[1050px]:hidden"></div>
-          <img
-            className="absolute top-0 right-10 z-20 max-[1050px]:hidden"
-            src={showcasehome}
-            alt=""
-          />
-        </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="w-full showcase text-white relative">
+              <div className="py-[51px] px-[99px] max-[670px]:px-5">
+                <h1 className="text-[40px] max-[450px]:text-[30px]  max-[330px]:text-[25px]">
+                  Беседки под ключ <br /> за 7 дней
+                </h1>
+              </div>
+              <div className="pb-[50px] relative z-10 max-[503px]:hidden">
+                <div className="flex items-center gap-14 text-[24px] py-[22px] px-[99px] max-[670px]:px-5 bg-[#ff7a1b]">
+                  <p>Выгода до 24%</p>
+                  <p>Сборка за 1 день</p>
+                </div>
+              </div>
+              <div className="h-[352px] w-[500px]  bg-white rounded-tl-full absolute right-0 top-0 max-[1050px]:hidden"></div>
+              <img
+                className="absolute top-0 right-10 z-20 max-[1050px]:hidden"
+                src={showcasehome}
+                alt=""
+              />
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="w-full showcase text-white relative">
+              <div className="py-[51px] px-[99px] max-[670px]:px-5">
+                <h1 className="text-[40px] max-[450px]:text-[30px]  max-[330px]:text-[25px]">
+                  Беседки под ключ <br /> за 7 дней
+                </h1>
+              </div>
+              <div className="pb-[50px] relative z-10 max-[503px]:hidden">
+                <div className="flex items-center gap-14 text-[24px] py-[22px] px-[99px] max-[670px]:px-5 bg-[#ff7a1b]">
+                  <p>Выгода до 24%</p>
+                  <p>Сборка за 1 день</p>
+                </div>
+              </div>
+              <div className="h-[352px] w-[500px]  bg-white rounded-tl-full absolute right-0 top-0 max-[1050px]:hidden"></div>
+              <img
+                className="absolute top-0 right-10 z-20 max-[1050px]:hidden"
+                src={showcasehome}
+                alt=""
+              />
+            </div>
+          </SwiperSlide>
+        </Swiper>
       </div>
     </div>
   );
